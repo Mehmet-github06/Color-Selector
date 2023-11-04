ac1e04690896cd86438a73ba50b1457081a17473
mehmet dogan <mehmetseymen91@gmail.com>
2023-11-04 16:58:41 +0300

    devam

--- app.js
+++ app.js
@@ -1,59 +1,54 @@
-function RandomColor(){
-    let items= "0123456789ABCDEF"
-    let color= "#"
-    for (let i=0 ; i<6; i++){
-        color+= items[Math.floor(Math.random()*16)];
+// Renk kodu oluşturan fonksiyon
+function getRandomColor() {
+    const hexChars = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+        color += hexChars[Math.floor(Math.random() * 16)];
     }
     return color;
-
 }
 
 
-const main = document.querySelector(".bgDiv")
-const inputbox=document.querySelector("#colorInput")
-const colorno= document.querySelector("#colorText")
-const clipboard=document.querySelector("#coppy")
-const h1 =document.getElementById("baslik")
 
 
-document.getElementById("ColorButton").addEventListener("click", () => {
-   
-    h1.style.color=inputbox.value= main.style.backgroundColor=colorno.textContent=RandomColor()
-    
-})
+// HTML öğelerini seçme
+const main = document.querySelector(".bgDiv");
+const inputbox = document.querySelector("#colorInput");
+const colorno = document.querySelector("#colorText");
+const clipboard = document.querySelector("#coppy");
+const h1 = document.getElementById("baslik");
 
+// Sayfa yüklendiğinde veya "Renk Al" düğmesine tıkladığında bir rastgele renk atama
+function assignRandomColor() {
+    const randomColor = getRandomColor();
+    h1.style.color = inputbox.value = main.style.backgroundColor = colorno.textContent = randomColor;
+}
 
+// "Renk Al" düğmesine tıklama olayı
+document.getElementById("ColorButton").addEventListener("click", assignRandomColor);
 
-document.getElementById("mouseover").addEventListener("mouseover", () => {
-   
-h1.style.color=inputbox.value= main.style.backgroundColor=colorno.textContent=RandomColor()
+// Fare üzerine gelindiğinde rastgele bir renk atama
+document.getElementById("mouseover").addEventListener("mouseover", assignRandomColor);
 
-    
-})
+// Sayfa yüklendiğinde başlangıçta bir rastgele renk atama
+window.addEventListener('load', assignRandomColor);
 
-window.addEventListener('load',()=>{
-    inputbox.value= main.style.backgroundColor=colorno.textContent=RandomColor()
-});
- 
-
-inputbox.addEventListener("input",()=>{
-   
-    colorno.innerText = inputbox.value
+// Input kutusundaki renk kodunu gösterme ve arka plan rengini güncelleme
+inputbox.addEventListener("input", () => {
+    colorno.innerText = inputbox.value;
     const selectColor = inputbox.value;
-    
-    main.style.backgroundColor = selectColor
-    
- })
+    main.style.backgroundColor = selectColor;
+});
 
+// Kopyala düğmesine tıklama olayı
+clipboard.addEventListener('click', () => {
+    const textToCopy = colorno.innerText;
+    navigator.clipboard.writeText(textToCopy)
+        .then(function () {
+            console.log("Renk kodu kopyalandı: " + textToCopy);
+        })
+        .catch(function (err) {
+            console.error("Kopyalama sırasında hata oluştu: " + err);
+        });
+});
 
- if (event.code === "Enter") {
-    
-    addButton.RandomColor()
-  }
-
-  clipboard.addEventListener('click',()=>{
-
-    navigator.clipboard.writeText(colorno.innerText)
- 
-    
- })
